d5821a18e26949cbfd668ff3d59f65997639a7c3
jim.stuttard <jims@stuttard.org>
2014-05-15 14:34:45 +0000

    First commit.

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,3 @@
-module Main where
-
-main :: IO ()
-main = quickHttpServe site
-
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
-
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
-
-We need to use the Snap.Http.Server.Env code instead:
-
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
@@ -27,7 +6,6 @@ import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server hiding (httpServe)
 import           Snap.Http.Server.Env
-
 main :: IO ()
 main = httpServe defaultConfig site
 
